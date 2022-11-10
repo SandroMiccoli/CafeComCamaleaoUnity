b51641dc3fd949203715e11b752f1a6e0fdcc4d8
Sandro Miccoli <smiccoli@gmail.com>
2022-11-10 11:29:12 -0300

    Added 360 videos and LavrasNovasTerrain

--- CafeComCamaleao/Assets/Scripts/ObjectSpawner.cs
+++ CafeComCamaleao/Assets/Scripts/ObjectSpawner.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
  public GameObject rockPrefab;
- // Start is called before the first frame update
+ 
+
  void Start()
  {
      StartCoroutine(SpawnRocks());
+     Debug.Log(transform.localScale.x);
  }
 
  IEnumerator SpawnRocks()
  {
      while(true)
      {
-         float randomTime = Random.Range(0.2f,1.5f);
-         float randomPosition = Random.Range(-10f,10f);
+         float randomTime = Random.Range(0.02f,0.5f);
+         Vector3 randomPosition = new Vector3(Random.Range(-10f,10f),Random.Range(-10f,10f),Random.Range(-10f,10f));
          
          yield return new WaitForSeconds(randomTime);
-         Instantiate(rockPrefab,new Vector3(randomPosition,transform.position.y,transform.position.z),Quaternion.identity);
+         Instantiate(rockPrefab,new Vector3(randomPosition.x,randomPosition.y,randomPosition.z),Quaternion.identity);
      }
  }
  
